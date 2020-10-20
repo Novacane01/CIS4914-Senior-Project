92e48f5e7e225ba54f4dcf94207496502b4613de
Novacane01 <mbionwuc@ufl.edu>
2020-10-20 14:51:51 -0400

    resolving merge conflicts

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,10 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-
-public class MainMenu : MonoBehaviour
-{
-    // Start is called before the first frame update
-
+public class MainMenu : MonoBehaviour {
+    public void playGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 }
