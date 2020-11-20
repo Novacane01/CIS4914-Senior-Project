aee97661a6dae46b6b6c00f68650639efb8f5341
AshAdames <adames.ashley@gmail.com>
2020-11-20 16:27:22 -0500

    changes 2

--- Assets/Scripts/UI/Config.cs
+++ Assets/Scripts/UI/Config.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System;
 
-<<<<<<< HEAD
+
 public class Config : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -23,16 +23,6 @@ public class Config : MonoBehaviour
     public Text taskNumText;
     public Text taskDurText;
     public GameObject popUpWindow;
-=======
-public class Config : MonoBehaviour {
-    // Start is called before the first frame update
-    public InputField popInput;
-    public InputField infectedInput;
-    public InputField transmissionInput;
-    public InputField numDaysInput;
-    public InputField taskNumInput;
-    public InputField taskDurInput;
->>>>>>> 60c6a530c02cead4a8c3b8d194295934bd094b39
 
     public static uint initPop = 0;
     public static uint initNumInfected = 0;
@@ -40,8 +30,6 @@ public class Config : MonoBehaviour {
     public static uint numDays = 3;
     public static uint tasksPerDay = 6;
     public static uint taskDuration = 5;
-
-<<<<<<< HEAD
 
     private void Update()
     {
@@ -70,32 +58,12 @@ public class Config : MonoBehaviour {
         }
         catch (FormatException e)
         {
-=======
-    private void ValueChange() {
-        //put numeric constraint
-        try {
-            initPop = Convert.ToUInt32(popInput.text.ToString());
-            initNumInfected = Convert.ToUInt32(infectedInput.text.ToString());
-            transRate = Convert.ToSingle(transmissionInput.text.ToString());
-            tasksPerDay = Convert.ToUInt32(taskNumInput.text.ToString());
-            taskDuration = Convert.ToUInt32(taskDurInput.text.ToString());
-            numDays = Convert.ToUInt32(numDaysInput.text.ToString());
-        }
-        catch (FormatException e) {
->>>>>>> 60c6a530c02cead4a8c3b8d194295934bd094b39
             Debug.Log("Given input is not a numeric value: " + e.ToString());
         }
 
     }
 
-<<<<<<< HEAD
-    public void OpenGame()
-    {
-        Debug.Log("wer here");
-=======
-
     public void OpenGame() {
->>>>>>> 60c6a530c02cead4a8c3b8d194295934bd094b39
         ValueChange();
         Debug.Log("Initial Population: " + initPop);
         Debug.Log("Initial Num Infected: " + initNumInfected);
@@ -103,7 +71,6 @@ public class Config : MonoBehaviour {
         Debug.Log("Number of days: " + numDays);
         Debug.Log("Tasks per day for each npc: " + tasksPerDay);
         Debug.Log("Task duration: " + taskDuration);
-<<<<<<< HEAD
         if (initPop == 0 || initNumInfected == 0 || transRate == 0 || numDays == 0)
         {
             Debug.Log("SOmethig went wronf");
@@ -111,12 +78,6 @@ public class Config : MonoBehaviour {
         }
         else
         {
-=======
-        if (initPop == 0 || initNumInfected == 0 || transRate == 0) {
-            Debug.Log("Popup window with stop message");
-        }
-        else {
->>>>>>> 60c6a530c02cead4a8c3b8d194295934bd094b39
             SceneManager.LoadScene(1);
         }
     }
